fe8b801ef679b7dd48e8a8b2fa1a7ffeb9861107
vkanellopoulos <vkanellopoulos@scytales.com>
2023-11-07 14:59:55 +0200

    no connectionMethod if both peripheral and central are set to false

--- transfer-manager/src/test/java/eu/europa/ec/eudi/iso18013/transfer/internal/DeviceRetrievalMethodExtTest.kt
+++ transfer-manager/src/test/java/eu/europa/ec/eudi/iso18013/transfer/internal/DeviceRetrievalMethodExtTest.kt
@@ -1,5 +1,6 @@
 package eu.europa.ec.eudi.iso18013.transfer.internal
 
+import com.android.identity.util.CborUtil
 import eu.europa.ec.eudi.iso18013.transfer.retrieval.BleRetrievalMethod
 import org.junit.Assert
 import org.junit.Test
@@ -12,6 +13,7 @@ class DeviceRetrievalMethodExtTest(
     private val peripheralServerMode: Boolean,
     private val centralClientMode: Boolean,
     private val clearBleCache: Boolean,
+    private val expectedMethodCount: Int,
 ) {
 
     companion object {
@@ -19,14 +21,14 @@ class DeviceRetrievalMethodExtTest(
         @JvmStatic
         fun data(): List<Array<Any>> {
             return listOf(
-                arrayOf(false, true, false),
-                arrayOf(false, false, false),
-                arrayOf(true, false, false),
-                arrayOf(true, true, false),
-                arrayOf(false, true, true),
-                arrayOf(false, false, true),
-                arrayOf(true, false, true),
-                arrayOf(true, true, true),
+                arrayOf(false, true, false, 1),
+                arrayOf(false, false, false, 0),
+                arrayOf(true, false, false, 1),
+                arrayOf(true, true, false, 1),
+                arrayOf(false, true, true, 1),
+                arrayOf(false, false, true, 0),
+                arrayOf(true, false, true, 1),
+                arrayOf(true, true, true, 1),
             )
         }
     }
@@ -35,10 +37,15 @@ class DeviceRetrievalMethodExtTest(
     fun testDeviceRetrievalMethodConnectionMethodsAndTransportOptions() {
         val bleRetrievalMethod =
             BleRetrievalMethod(peripheralServerMode, centralClientMode, clearBleCache)
-        val connectionMethod = bleRetrievalMethod.connectionMethod
-        Assert.assertEquals(1, connectionMethod.size)
+        val connectionMethods = bleRetrievalMethod.connectionMethod
+        Assert.assertEquals(expectedMethodCount, connectionMethods.size)
+
         val transportOptions = listOf(bleRetrievalMethod).transportOptions
         Assert.assertEquals(clearBleCache, transportOptions.bleClearCache)
+
+        val connectionMethod = connectionMethods.firstOrNull()
+        CborUtil.toDiagnostics(connectionMethod?.toDeviceEngagement() ?: byteArrayOf())
+            .also { println("DeviceEngagement: $it") }
     }
 
 }
