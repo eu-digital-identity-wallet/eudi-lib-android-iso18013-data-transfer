0294e639d91947b37f8002a237e9f5c5d3f4fe95
Henry Jalonen <henry.jalonen@reaktor.com>
2025-10-07 17:05:22 +0300

    Update kotlinx-datetime and Multipaz
    - Update kotlinx-datetime to 0.7.1 and opt in to the experimental native kotlin.time API, as required by the new version of the library.
    - Update Multipaz to 0.93.0, as it has also moved to the latest version of kotlinx-datetime.

--- transfer-manager/build.gradle.kts
+++ transfer-manager/build.gradle.kts
@@ -101,6 +101,12 @@ android {
     }
 }
 
+kotlin {
+    compilerOptions {
+        optIn.add("kotlin.time.ExperimentalTime")
+    }
+}
+
 dependencies {
     implementation(libs.appcompat)
     implementation(libs.eudi.document.manager)
