683504f215df78beefa66a243605a10454c1b42d
Marina Ioannou <ioannoum@scytales.com>
2026-05-06 16:17:56 +0300

    Update Dokka to 2.2.0 and migrate documentation tasks

--- transfer-manager/build.gradle.kts
+++ transfer-manager/build.gradle.kts
@@ -19,6 +19,8 @@ import com.github.jk1.license.filter.LicenseBundleNormalizer
 import com.github.jk1.license.filter.ReduceDuplicateLicensesFilter
 import com.github.jk1.license.render.InventoryMarkdownReportRenderer
 import com.vanniktech.maven.publish.AndroidMultiVariantLibrary
+import org.jetbrains.dokka.gradle.formats.DokkaFormatPlugin
+import org.jetbrains.dokka.gradle.internal.InternalDokkaGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.util.Locale
 
@@ -139,26 +141,40 @@ dependencyCheck {
 
 // Dokka generation
 
-tasks.dokkaGfm.configure {
-    val outputDir = file("$rootDir/docs")
-    doFirst { delete(outputDir) }
-    outputDirectory.set(outputDir)
+@OptIn(InternalDokkaGradlePluginApi::class)
+abstract class DokkaMarkdownPlugin : DokkaFormatPlugin(formatName = "markdown") {
+    override fun DokkaFormatPlugin.DokkaFormatPluginContext.configure() {
+        project.dependencies {
+            dokkaPlugin(dokka("gfm-plugin"))
+            formatDependencies.dokkaPublicationPluginClasspathApiOnly
+                .dependencies.addLater(dokka("gfm-template-processing-plugin"))
+        }
+    }
+}
+
+apply<DokkaMarkdownPlugin>()
+
+dokka {
+    moduleName.set("transfer-manager")
+    dokkaPublications.named("markdown") {
+        outputDirectory.set(rootDir.resolve("docs"))
+    }
+}
+
+tasks.named("dokkaGeneratePublicationMarkdown").configure {
+    doFirst { delete(rootDir.resolve("docs")) }
+}
+
+tasks.named("dokkaGenerateMarkdown").configure {
+    group = "documentation"
 }
 
 tasks.register<Jar>("dokkaHtmlJar") {
     group = "documentation"
     description = "Assembles HTML documentation with Dokka."
-    dependsOn(tasks.dokkaHtml)
-    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
+    dependsOn("dokkaGeneratePublicationHtml")
+    from(tasks.named("dokkaGeneratePublicationHtml"))
     archiveClassifier.set("html-docs")
-}
-
-tasks.register<Jar>("dokkaJavadocJar") {
-    group = "documentation"
-    description = "Assembles Javadoc documentation with Dokka."
-    dependsOn(tasks.dokkaJavadoc)
-    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
-    archiveClassifier.set("javadoc")
 }
 
 // Third-party licenses report
@@ -189,7 +205,7 @@ tasks.generateLicenseReport.configure {
 tasks.register<Task>("buildDocumentation") {
     group = "documentation"
     description = "Builds the documentation and license report."
-    dependsOn("dokkaGfm", "generateLicenseReport")
+    dependsOn("dokkaGenerateMarkdown", "generateLicenseReport")
 }
 tasks.assemble.configure {
     finalizedBy("buildDocumentation")
